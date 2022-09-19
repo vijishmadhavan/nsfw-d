eebfcf6a1354dc154eb976379eba72c776bef8f1
Vijish Madhavan <53169213+vijishmadhavan@users.noreply.github.com>
2022-09-19 14:12:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const threshold = 0.75;
 async function load_model() {
     // It's possible to load the model locally or from a repo
     // You can choose whatever IP and PORT you want in the "http://127.0.0.1:8080/model.json" just set it before in your https server
-    const model = await loadGraphModel("http://127.0.0.1:8080/model.json");
+    //const model = await loadGraphModel("http://127.0.0.1:8080/model.json");
     const model = await nsfwjs.load("https://raw.githubusercontent.com/vijishmadhavan/nsfw-detector/master/models/model.json");
     
     return model;
